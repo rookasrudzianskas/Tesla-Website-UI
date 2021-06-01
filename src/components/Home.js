42c5a436be2854e66d7362bf0cde6aa28fc5db27
Rokas <rookas.rudzenskas@gmail.com>
2021-06-01 18:47:25 +0300

    Rokas

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Home = () => {
+    return (
+        <Container>
+            <Header>
+
+            </Header>
+        </Container>
+    );
+};
+
+export default Home;
+
+
+const Container = styled.div`
+  height: 100vh;  
+`

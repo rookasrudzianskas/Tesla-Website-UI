b842582bdc070b318bab3b54669897cf0c22615b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-03 11:08:12 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,54 @@ import styled from "styled-components";
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <a href="">
+                <img src="/images/logo.svg" alt=""/>
+            </a>
+
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+
+            </Menu>
+
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+            </RightMenu>
+        </Container>
     );
 };
 
 export default Header;
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+`
+
+const RightMenu = styled.div`
+  
+`
